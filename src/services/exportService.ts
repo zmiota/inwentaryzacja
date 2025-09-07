77647b691cba390c60d3035de9ada313d05c12ca
zmiota <119612696+zmiota@users.noreply.github.com>
2025-09-07 13:56:03 +0200

    Fix jsPDF named import

--- src/services/exportService.ts
+++ src/services/exportService.ts
@@ -1,5 +1,5 @@
 import { FinalInventoryEntry, Inventory, CommissionMember } from '../types';
-import jsPDF from 'jspdf';
+import { jsPDF } from 'jspdf';
 import autoTable from 'jspdf-autotable';
 
 // Register the autoTable plugin

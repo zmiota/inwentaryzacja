4e82e167271347943b5c00e55b0a45bf2146808a
zmiota <119612696+zmiota@users.noreply.github.com>
2025-10-04 17:33:42 +0200

    Updated exportService.ts

--- src/services/exportService.ts
+++ src/services/exportService.ts
@@ -61,7 +61,7 @@ uniwersalny`;
       // Prawa kolumna
       const rightColumnX = pageWidth / 2 + 5;
       let rightY = 33;
-      const title2 = `Rodzaj inwentaryzacji: końcowa - ${inventory.name}`;
+      const title2 = `    SRodzaj inwentaryzacji: końcowa - ${inventory.name}`;
       doc.text(title2, pageWidth / 2, currentY);
      // doc.text('Rodzaj inwentaryzacji: końcowa - ${inventory.name}', rightColumnX, rightY);
       rightY += 6;
